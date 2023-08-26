7ee1b3008392ee38c5e9f49bc45117a37863e4a4
kishore9251 <141656404+kishore9251@users.noreply.github.com>
2023-08-26 20:00:14 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "Kishore M",
+      "email": "krishkishore9251@gmail.com",
+      "phone": 8489589233,
+      "degree": "B.E",
+      "location": {
+        "address": "109/15b Ashok nagar,",
+        "postalCode": 638656,
+        "city": "Dharapuram",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/sankaranarayanan-saminathan-675359a0/",
+          "github":"https://github.com/Kishore9251"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Aquasub Engineering",
+        "position": "Production incharge",
+        "startDate": "2014-06-02",
+        "endDate": "2018-11-21",
+        "summary": "i am the production incharge handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Karpagam university, coimbatore",
+        "department": "Mechanical",
+        "studyType": "Part time",
+        "batch start year": 2015,
+        "batch end year": 2018,
+        "gpa": 6.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "HTML,javascript",
+        "level": "beginer",
+        "project": [
+          "random number show in html site using javscript"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "Game": "Football playing",
+      }
+    ]
+  }
+  console.log(myResume);
